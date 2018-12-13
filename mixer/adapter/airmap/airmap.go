dd2eac7f0f8671ad3e811942348e59f051feb37a
Thomas Voß <thomas@airmap.com>
2018-12-13 20:56:33 +0100

    [airmap] Add missing error variable

--- mixer/adapter/airmap/airmap.go
+++ mixer/adapter/airmap/airmap.go
@@ -63,6 +63,8 @@ var (
 		access.CodeUnauthorized:  rpc.UNAUTHENTICATED,
 		access.CodeQuotaExceeded: rpc.RESOURCE_EXHAUSTED,
 	}
+
+	errFailedClosingStream = errors.New("failed to close log access stream")
 )
 
 func init() {
